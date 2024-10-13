8b37d145a717692f9612bd11e709ba1461b83f2f
Aruna <suri1aruna@gmail.com>
2024-10-13 18:54:41 -0400

    sunday work

---
+++ Relationaloperator.java
@@ -0,0 +1,18 @@
+package oct13th2024;
+
+public class Relationaloperator {
+    public static void main(String args[])
+    {
+        int x=24;
+        int y=35;
+        boolean result=x>y && y<=40;
+        System.out.println(result);
+        boolean result1=x<=y || y<=40;
+        System.out.println(result1);
+        boolean result2= x<=30;
+        System.out.println(!result2);
+
+
+
+    }
+}
---
+++ ifelseif.java
@@ -0,0 +1,18 @@
+package oct13th2024;
+
+public class ifelseif {
+    public static void main(String args[])
+    {
+        int x=10;
+        int y=15;
+        int z=20;
+        if(x>y&&x>z)
+            System.out.println(x );
+        else if (y>x&&y>z)
+            System.out.println(y);
+        else
+            System.out.println(z);
+
+        }
+    }
+
