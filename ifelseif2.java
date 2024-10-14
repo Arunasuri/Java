2088f11a682e732036d7fab92317df453afaea64
Aruna <suri1aruna@gmail.com>
2024-10-13 20:40:55 -0400

    added

---
+++ ifelseif2.java
@@ -0,0 +1,17 @@
+package oct13th2024;
+
+public class ifelseif2 {
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=20;
+        int c=30;
+        if (a>b&&a>c)
+            System.out.println(a);
+        else if(b>c)
+            System.out.println(b);
+        else
+            System.out.println(c);
+
+    }
+}
---
+++ ternaryoperator.java
@@ -0,0 +1,12 @@
+package oct13th2024;
+
+public class ternaryoperator {
+    public static void main(String args[])
+    {
+        int x=20;
+        int result=0;
+        result =x%2==0 ? 10 :100;
+        System.out.println(result);
+
+    }
+}
