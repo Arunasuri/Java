14af908576698be5664691d8c43da37bb08218e7
Aruna <suri1aruna@gmail.com>
2024-10-14 10:54:33 -0400

    switch block

---
+++ switch1.java
@@ -0,0 +1,36 @@
+package oct13th2024;
+
+public class switch1 {
+    public static void main(String args[])
+    {
+            int n=5;
+            switch (n)
+            {
+                case 1:
+                    System.out.println("sunday");
+                    break;
+                case 2:
+                    System.out.println("Monday");
+                    break;
+                case 3:
+                    System.out.println("Tuesday");
+                    break;
+                case 4:
+                    System.out.println("wednesday");
+                    break;
+                case 5:
+                    System.out.println("Thursday");
+                    break;
+                case 6:
+                    System.out.println("Friday");
+                    break;
+                case 7:
+                    System.out.println("saturday");
+                    break;
+                default:
+                    System.out.println("Enter valid information");
+
+            }
+    }
+}
+
